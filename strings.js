772f69b0c23b540961307b1b70348321764c5aad
shubha <shubha07658@gmail.com>
2021-10-12 21:03:43 +0530

    strings

---
+++ strings.js
@@ -0,0 +1,22 @@
+var myString = 'Javascript'
+console.log(typeof(myString))
+
+var secondString = 'javascript is programming language'
+console.log(typeof(secondString))
+
+// what if string contains double quotes
+
+var doubleQuotesString = "We are \"going\" learn\"Javascript\""
+
+console.log(doubleQuotesString)
+
+// what if string contains single quotes
+
+var singleQuotesString = 'We are \'going\' learn \'Javascript\''
+console.log(singleQuotesString)
+
+var doubleQuotesString2 = "We are 'going' learn 'Javascript'"
+var singleQuotesString2 = 'We are "going" learn "Javascript"'
+
+console.log(doubleQuotesString2)
+console.log(singleQuotesString2)
